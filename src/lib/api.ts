d97dc3cf7b3f7fddcd9e1c1b67a01688e8651bb1
abbos-21 <abbos.nazariy0621@gmail.com>
2026-01-09 13:59:11 +0500

    commit 19

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -38,7 +38,7 @@ api.interceptors.response.use(
 
       try {
         const refreshToken = localStorage.getItem('refreshToken')
-        const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/refresh`, {
+        const res = await axios.post(`${import.meta.env.VITE_API_URL}/auth/refresh`, {
           refreshToken,
         })
 
