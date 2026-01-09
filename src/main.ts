1c4479e7928c4b90b9d8260978b0c6b4186e49ae
abbos-21 <abbos.nazariy0621@gmail.com>
2026-01-09 13:55:32 +0500

    commit 18

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { init } from '@tma.js/sdk-vue'
 init()
+import { syncAuth } from './services/authService'
 
 import './assets/main.css'
 import { createApp } from 'vue'
+syncAuth()
+  .catch(() => {
+    console.warn('Telegram auth failed')
+  })
+  .finally(() => {
+    app.mount('#app')
+  })
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
