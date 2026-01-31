d1024fda744a67f8b7314dee951d081a7f726a20
abbos-21 <abbos.nazariy0621@gmail.com>
2026-01-31 18:43:26 +0500

    commit 27

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -22,7 +22,7 @@ api.interceptors.response.use(
   async (error) => {
     const original = error.config
 
-    if (error.response?.status === 403 && !original._retry) {
+    if (error.response?.status === 401 && error.response?.status === 403 && !original._retry) {
       original._retry = true
 
       if (isRefreshing) {
